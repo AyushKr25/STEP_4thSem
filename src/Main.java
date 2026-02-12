c605a0bc66bc5628ac3cb3b5bc4837d80e0a2ac6
AyushKr25 <ayushak1925kr@gmail.com>
2026-02-12 15:11:07 +0530

    Commit check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Ayush!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+        sc.close();
     }
 }
